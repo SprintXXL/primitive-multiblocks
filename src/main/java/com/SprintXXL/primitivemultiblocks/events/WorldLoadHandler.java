e9765f6761a75ab5636a5a77796308454a138fbd
SprintXXL <mspmain@pm.me>
2026-06-23 02:33:46 +0200

    .

--- src/main/java/com/SprintXXL/primitivemultiblocks/events/WorldLoadHandler.java
+++ src/main/java/com/SprintXXL/primitivemultiblocks/events/WorldLoadHandler.java
@@ -1,5 +1,6 @@
 package com.SprintXXL.primitivemultiblocks.events;
 
+import com.SprintXXL.primitivemultiblocks.formation.FormedMultiblockManager;
 import com.SprintXXL.primitivemultiblocks.formation.world.MultiblockWorldData;
 import net.minecraft.world.World;
 import net.minecraftforge.event.world.WorldEvent;
@@ -20,6 +21,23 @@ public class WorldLoadHandler {
             return;
         }
 
+        FormedMultiblockManager.clear();
         MultiblockWorldData.get(world).restore(world);
+    }
+
+    @SubscribeEvent
+    public void onWorldUnload(WorldEvent.Unload event) {
+
+        World world = event.getWorld();
+
+        if (world.provider.getDimension() != 0) {
+            return;
+        }
+
+        if (world.isRemote) {
+            return;
+        }
+
+        FormedMultiblockManager.clear();
     }
 }
