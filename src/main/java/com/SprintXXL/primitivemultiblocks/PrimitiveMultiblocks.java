a308eb8ea7770475738d415bb57ecfab4847fb08
SprintXXL <mspmain@pm.me>
2026-07-16 04:18:13 +0200

    .

--- src/main/java/com/SprintXXL/primitivemultiblocks/PrimitiveMultiblocks.java
+++ src/main/java/com/SprintXXL/primitivemultiblocks/PrimitiveMultiblocks.java
@@ -7,15 +7,11 @@ import com.SprintXXL.primitivemultiblocks.multiblocks.MultiblockRegistry;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import static com.SprintXXL.primitivemultiblocks.Reference.*;
 
 @Mod(modid = MODID, name = NAME, version = VERSION)
 public class PrimitiveMultiblocks {
-
-    public static final Logger LOGGER = LogManager.getLogger(NAME);
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
